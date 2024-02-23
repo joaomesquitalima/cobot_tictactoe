fb879e49c5723b89e4fab50937b16c2150488e9d
João Vitor Mesquita de Lima <99664194+joaomesquitalima@users.noreply.github.com>
2024-02-23 09:11:59 -0400

    Update main.py

--- robo_tictactoe/main.py
+++ robo_tictactoe/main.py
@@ -1,8 +1,9 @@
 from pyModbusTCP.client import ModbusClient
 from pyModbusTCP import utils
 from time import sleep
-from jogo_da_velha import criarBoard,printBoard,getInputValido, enserirValor, verificaGanhador
+from jogo_da_velha import criarBoard,printBoard,verificaGanhador
 from minimax import movimentoIA, movimentoRobo
+# from tocar import play_audio
 SERVER_IP = '192.168.1.254'
 SERVER_PORT = 502
 DEVICE_ID = 1
@@ -20,68 +21,60 @@ else:
 play_pause = client.write_single_coil(7104,1)
 
 sleep(1)
-jogador = 0
+jogador = 1
+
+#inicia um board vazio
 board = criarBoard([2,2,2,2,2,2,2,2,2])
 
-client.write_single_register(9000,0)
-
-def analisar():
-    lista = []
-    while(len(lista) <=9):
-        if sinal_verde:
-            lista.append(1)
-        elif sinal_vermelho:
-            lista.append(0)
-        elif sinal_laranja:
-            lista.append(2)
-
-    return lista
 
 ganhador = None
+status = 0
+
+client.write_single_register(9000,0)
+client.write_single_register(9001,0)
+
+turno = 0
 
 while True:
-    green_button = client.read_discrete_inputs(3,1)[0]
-    red_button = client.read_discrete_inputs(4,1)[0]
-
-    sinal_verde = client.read_coils(4,1)[0]
-    sinal_vermelho = client.read_coils(5,1)[0]
-    sinal_laranja = client.read_coils(6,1)[0]
-
-    printBoard(board)
-    if jogador == 0:
-        print("Sua vez jogador")
-        i = getInputValido("Insira a linha: ")
-        j = getInputValido("Insira a coluna: ")
-        r ,board = enserirValor(board,i,j,jogador)
-    
-    if jogador ==1:
-        print("Sua vez computador")
-        # lista = analisar()
-        # board = criarBoard(lista)
-        i,j = movimentoIA(board,jogador)
-        r ,board = enserirValor(board,i,j,jogador)
-        pos = movimentoRobo(i,j)
-        client.write_single_register(9000,pos)
+    print('Aguardando botão...')
+    while client.read_holding_registers(9002,1)[0] == 0:
+        pass
         
+    print(turno)
+        
+    # play_audio("minha_vez.mp3")
+    # # # aguarda o fim da analise
+        
+    #recebe lista do robo
+    lista_tm = client.read_holding_registers(9003, 18)
+    
+    print(lista_tm)
+    lista = []
 
-    if r:
-        jogador = (jogador+1)%2
-
+    #filtra a lista recebida do robo
+    for i in range(len(lista_tm)):
+        if i % 2 == 1:
+            lista.append(lista_tm[i])
+    #cria board apartir da lista
+    print('Tabuleiro:', lista)
+    board = criarBoard(lista)
+    # printBoard(board)
     ganhador = verificaGanhador(board)
+
+    # if ganhador:
+    #     #altera status do jogo
+    #     client.write_single_register(9002,pos)
+        
+    #devolve linha i e coluna j do board
+    i,j = movimentoIA(board,jogador)
+
+    #usa a linha e coluna pra devolver um numero que representa um ponto pro robo
+    pos = movimentoRobo(i,j)
     
+    # o robo recebe a variavel pos , onde irá ir pra um ponto no tabuleiro fisico
+    client.write_single_register(9000,pos)
 
-    if ganhador:
-        printBoard(board)
-        break
-    
-
-if ganhador == "X":
-    print("Voce ganhou")
-    
-
-if ganhador == "O":
-    print("O computador venceu")
-    client.write_single_register(9000,22)
+    # client.write_single_register(9002,0)
 
 
-    
+    sleep(1)
